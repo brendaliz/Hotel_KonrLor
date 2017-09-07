cfc34b6813b46670d7793d417f926584522c8c97
brendaliz <brendacanov@gmail.com>
2017-09-07 17:38:12 -0500

    Usuario Enity
    Creacion del usuario

---
+++ src/main/java/co/konrad/hotel_konrlor/entities/UsuarioEntity.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.konrad.hotel_konrlor.entities;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author brend
+ */
+@Entity
+public class UsuarioEntity implements Serializable{
+    
+     private static final long serialVersionUID = 1L;
+    
+    @Id
+    @Column(name="id_usuario")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @Column(name = "nombre_usuario", nullable = false)
+    private String nombre;
+     
+    @Column(name = "apellido_usuario", nullable = false)
+    private String apellido;
+    
+    private String contraseña;
+    private double avatar;
+    private String correo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public double getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(double avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    
+    
+    
+}
