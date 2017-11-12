1316dfa9bf023dc6e1963bad8ef8c579c342cf7e
brendaliz <brendacanov@gmail.com>
2017-11-12 18:36:04 -0500

    Crear usuario js

--- src/main/webapp/js/crearUsuario.js
+++ src/main/webapp/js/crearUsuario.js
@@ -1,7 +1,80 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+(function ($) {
+    
+    var getURLParameter = function getUrlParameter(sParam){
+      var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+      for(i=0; i<sURLVariables.length;i++){
+          sParameterName = sURLVariables[i].split('=');
+          
+          if(sParameterName[0] === sParam){
+              return sParameterName[1] ===undefined ? true: sParameterName[1];
+          }
+      }
 
+    };
+    
+    if(getURLParameter('id')){
+        //Codigo Actualizar
+        $.ajax({
+            url: '/Hotel_KonrLor/api/Usuarios' + getURLParameter("id"),
+            method: 'GET',
+            contentType: 'application/json',
+            dataType:'json'
+        }).done(function(data){
+            var nombre = $('#nombre').val(data.nombre);
+            var contraseña = $('#contraseña').val(data.patrocinador);
+            var correo = $('#correo').val(data.duracionDias/*nombre que le dimos en DTO*/);
+            var id= data.id;
+            
+            $('#crearBoton').text('Actualizar Usuario').click(function(event){
+            var nombre = $('#nombre').val();
+            var contraseña = $('#contraseña').val();
+            var correo = $('#correo').val(/*nombre que le dimos en DTO*/);
+            $.ajax({
+               url: '/Hotel_KonrLor/api/Usuarios'+id,
+               method:'PUT',
+               data: JSON.stringify({
+                   nombre: nombre,
+                    contraseña: contraseña,
+                    correo: correo,
+                    id:id
+               }),
+               contentType: 'application/json',
+               dataType: 'json'                
+            }).done(function(data){
+                window.location.href= '/Hotel_KonrLor';
+            }).fail(function (xhr, status, error){
+                console.log(error);
+            });
+            });
+        }).fail(function (xhr, status, error){
+            console.log(error);
+        });
+    }else{
+        //Codigo crear
+        $('#crearBoton').click(function (event) {
+            var nombre = $('#nombre').val();
+            var contraseña = $('#contraseña').val();
+            var correo = $('#correo').val();
+
+            $.ajax({
+                url: '/Hotel_KonrLor/api/Usuarios',
+                method: 'POST',
+                data: JSON.stringify({
+                    nombre: nombre,
+                    contraseña: contraseña,
+                    correo: correo
+                }),
+                contentType: 'application/json',
+                dataType: 'json'
+            }).done(function (data) {
+                window.location.href = '/Hotel_KonrLor';
+            }).fail(function (xhr, status, error) {
+                console.log(error);
+            });
+        });        
+    }
+})(jQuery);
 
--- target/Hotel_KonrLor-1.0-SNAPSHOT/js/usuario.js
+++ target/Hotel_KonrLor-1.0-SNAPSHOT/js/usuario.js
@@ -7,10 +7,56 @@
            contentType: 'application/json',
            dataType: 'json'
        }).done(function(data){
-           
+        for(var i=0; i<data.length; i++){
+                              
+               var tableRow = $('<tr>');
+               //Crear una variable por cada division/columna de la tabla
+               var nombreUsuario = $('<td>');
+               nombreUsuario.text(data[i].nombre);
+               var contraseña = $('<td>');
+               contraseña.text(data[i].contraseña);
+               var correo = $('<td>');
+               correo.text(data[i].correo);
+               
+               var acciones = $('<td>');
+               var botonEliminar = $('<button>').addClass('btn btn-danger');
+               //Se crea un atributo que realiza el transporte de datos
+               //data-'cualquierNombre'
+               botonEliminar.text('Eliminar').attr('data-id', data[i].id);
+               var botonActualizar = $('<a href="/Prueba1/usuarioCreate.html?id='+ data[i].id+'">').addClass('btn btn-primary');
+               botonActualizar.text('Actualizar');
+               
+               botonEliminar.click(eliminar);
+               acciones.append(botonActualizar);
+               acciones.append(botonEliminar);
+               
+               //Agregar las columnas a la fila
+               tableRow.append(nombre);
+               tableRow.append(contraseña);
+               tableRow.append(correo);
+               tableRow.append(acciones); 
+               
+               console.log($('#tFestivales table tbody'));
+               $('#tFestivales table tbody').append(tableRow);
+           }
+           console.log(data);
+       }).fail(function (xhr, status, error){
+           console.log(error);
+       });       
+   });
+   
+   function eliminar(event){
+       $.ajax({
+           url: '/Hotel_KonrLor/api/Usuarios' +$(this).attr('data-id'),
+           method: 'DELETE',
+           contentType: 'application/json',
+           dataType: 'json'
+       }).done(function(data){
+           console.log('Elimino');
+           window.location.href = '/Hotel_KonrLor';
        }).fail(function(xhr,status,error){
-           
-       }); 
-       
-   });   
+           console.log(error);
+       });
+   }
 })(jQuery);
+
